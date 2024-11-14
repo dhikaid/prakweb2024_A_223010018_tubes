a0665274c8db2da1647271ecc3ac4b62df9ff63d
aureliare <aureliamel123@gmail.com>
2024-11-14 21:05:24 +0700

    feat: Halaman login user, Halaman regist user, Halaman lupa password, Halaman reset password

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/regist', function () {
+    return view('regist');
+});
+
+Route::get('/reset-password', function () {
+    return view('reset-password');
+});
+
+Route::get('/lupa-password', function () {
+    return view('lupa-password');
+});
