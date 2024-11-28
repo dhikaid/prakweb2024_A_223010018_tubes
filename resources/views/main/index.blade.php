4b2d25d28ed419a346b94c1b9433ab3907ec86fa
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-11-28 18:40:24 +0700

    style: halaman depan

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -21,7 +21,7 @@
     </div>
 
 
-    <div class="mt-10">
+    <div class="my-10">
         <div class="flex justify-between">
             <h1 class="font-bold text-xl mb-7">Creator Event</h1>
             <a class="text-sm mb-7 text-blue-600">Lihat semua</a>
@@ -107,5 +107,191 @@
         </div>
     </div>
 
+    <div class="my-10">
+        <div class="flex justify-between">
+            <h1 class="font-bold text-xl mb-7">Event Terbaru</h1>
+            <a class="text-sm mb-7 text-blue-600">Lihat semua</a>
+        </div>
+        <div class="flex justify-center items-center gap-5">
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+    </div>
+    <div class="my-10">
+        <div class="flex justify-between">
+            <h1 class="font-bold text-xl mb-7">Event di <span class="text-blue-600">Bandung</span></h1>
+            <a class="text-sm mb-7 text-blue-600">Lihat semua</a>
+        </div>
+        <div class="flex justify-center items-center gap-5">
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href=""
+                class="uppercase w-80 overflow-hidden border border-gray-300 inline-block rounded-xl rounded-tl-3xl">
+                <img src="https://assets.loket.com/neo/production/images/banner/7V8f2_1730377351514268.jpeg"
+                    class="rounded-tl-3xl rounded-br-3xl w-80  mb-3">
+                <div class="px-3 pb-3">
+                    <p class="font-bold text-lg">INTRODUCTION GDGOCUNPAS</p>
+                    <p class="font-semibold text-lg text-gray-500 mb-2">23 DEC 2024</p>
+                    <p class="text-2xl font-bold">Rp. 50.000</p>
+                    <div class="img mt-3 flex items-center  gap-3">
+                        <img src="https://assets.loket.com/neo/production/images/organization/20220815105547.jpg"
+                            class="w-7 rounded-full" alt="">
+                        <p class="flex justify-center items-center gap-1">GDGOCUNPAS <svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" class="size-4">
+                                <path fill-rule="evenodd"
+                                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+    </div>
 </div>
 @endsection
