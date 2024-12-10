0bfa20d569bfae3a7624169fa70f4d177adff44e
aureliare <aureliamel123@gmail.com>
2024-12-10 23:49:48 +0700

    feat: roles

--- resources/views/dashboard/layouts/partials/sidebartemplate.blade.php
+++ resources/views/dashboard/layouts/partials/sidebartemplate.blade.php
@@ -46,6 +46,19 @@
                 <span class="mx-4 font-medium">Users</span>
             </a>
 
+            <a class="flex items-center px-4 py-2 mt-5 {{ Request::is('dashboard/roles*') ? 'text-white bg-indigo-500 hover:bg-indigo-700' : 'text-gray-600 hover:bg-gray-100 hover:text-gray-700' }} transition-colors duration-300 transform rounded-md "
+                href="/dashboard/roles">
+                <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+                <span class="mx-4 font-medium">Roles</span>
+            </a>
+
             <a class="flex items-center px-4 py-2 mt-5 {{ Request::is('dashboard/events*') ? 'text-white bg-indigo-500 hover:bg-indigo-700' : 'text-gray-600 hover:bg-gray-100 hover:text-gray-700' }} transition-colors duration-300 transform rounded-md"
                 href="/dashboard/events">
                 <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
