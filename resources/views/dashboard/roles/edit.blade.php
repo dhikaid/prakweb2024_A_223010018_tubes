9d3532b6f09202bac8b65da9f1b3496f9457cbd5
aureliare <aureliamel123@gmail.com>
2024-12-11 18:33:02 +0700

    feat: halaman edit roles

---
+++ resources/views/dashboard/roles/edit.blade.php
@@ -0,0 +1,97 @@
+@extends('dashboard.layouts.main')
+@section('main')
+
+{{-- TULIS CODE DISINI --}}
+
+<section class="users">
+    <div class="flex flex-col">
+        <div class="info my-4">
+            @include('dashboard.layouts.partials.breadcumb',[$datas = ['Users', strtolower($user->username) , 'Edit']])
+            <div class="flex items-center gap-3 my-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
+                    <path
+                        d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                </svg>
+
+                <div class="flex items-center gap-1">
+                    <h1 class="text-2xl md:text-3xl uppercase font-bold">Edit User: {{ $user->username }}</h1>
+                    @if ($user->isVerified)
+                    @include('layouts.partials.verified')
+                    @endif
+                </div>
+            </div>
+        </div>
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                <div class="overflow-hidden md:rounded-lg">
+                    <div class=" rounded-lg flex items-start justify-between">
+                        <div class="w-full">
+                            <form class="space-y-5" method="POST" action="/dashboard/users/{{ $role->uuid }}"
+                                enctype="multipart/form-data"
+                                x-data="{verified:{{ $user->role->role === 'EO' ? 'true' : 'false' }}}">
+                                @method('PUT')
+                                @csrf
+                                          
+                                {{-- START ROLE --}}
+                                <div>
+                                    <label for="roles"
+                                        class="block text-sm text-black font-semibold dark:text-gray-300">Roles</label>
+
+                                    <div class="relative flex items-center mt-2">
+                                        <select id="countries"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            name="role"
+                                            x-on:change="verified = $event.target.options[$event.target.selectedIndex].text === 'EO'"
+                                            required>
+                                            @foreach ($roles as $role)
+                                            <option value="{{ $role->uuid }}" {{ old('role', $user->
+                                                role->uuid)===$role->uuid ? 'selected'
+                                                : '' }}>
+                                                {{ $role->role }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+
+                                    </div>
+                                    @error('role') <p class="mt-3 text-xs text-red-400">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                {{-- ENDFORM --}}
+                                {{-- START ROLE --}}
+                                <div x-show="verified">
+                                    <label for="roles"
+                                        class="block text-sm text-black font-semibold dark:text-gray-300">Verified</label>
+                                    <div class="relative flex items-center mt-2">
+                                        <select id="countries"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            name="isVerified">
+                                            <option value="false">Tidak</option>
+                                            <option value="true" {{ $user->isVerified ? 'selected' :'' }}>Verified
+                                            </option>
+                                        </select>
+                                    </div>
+                                    @error('isVerified') <p class="mt-3 text-xs text-red-400">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                {{-- ENDFORM --}}
+
+                                <button
+                                    class="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                    </svg>
+                                    <span class="mx-1">Update</span>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+{{-- END TULIS CODE --}}
+
+@endsection
