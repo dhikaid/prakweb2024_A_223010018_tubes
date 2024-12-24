f9c68278ee0dbc629ffe323de1b02e80a6284bab
AzharLuthfi <azhar.luthfiadi@gmail.com>
2024-12-24 19:42:36 +0700

    fix: api creator events

--- app/Http/Controllers/Api/CreatorEventController.php
+++ app/Http/Controllers/Api/CreatorEventController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
+
 //import resource PostResource
 use App\Http\Resources\CreatorEventResource;
 
@@ -15,20 +16,24 @@ class CreatorEventController extends Controller
 {
     public function index()
     {
-        $data = DB::table('events')
-            ->join('users', 'events.user_uuid', '=', 'users.uuid')
-            ->join('roles', 'users.role_uuid', '=', 'roles.uuid')
-            ->where('roles.role', 'EO') // Filter hanya untuk role EO
-            ->select('users.fullname as creator_name', 'events.name as event_name')
-            ->get();
+        return response()->json(['message' => 'Route is working']);
+
+
+        // jika ingin menggunakan DB
+        // $data = DB::table('events')
+        //     ->join('users', 'events.user_uuid', '=', 'users.uuid')
+        //     ->join('roles', 'users.role_uuid', '=', 'roles.uuid')
+        //     ->where('roles.role', 'EO') // Filter hanya untuk role EO
+        //     ->select('users.fullname as creator_name', 'events.name as event_name')
+        //     ->get();
 
         // jika ingin menggunakan model
-        // $events = Event::with('user.role')
-        // ->whereHas('user.role', function ($query) {
+        // $data = Event::with('creator.role')
+        // ->whereHas('creator.role', function ($query) {
         //     $query->where('role', 'EO');
         // })
         // ->get();
 
-        return new CreatorEventResource(true, 'List Data Posts', $data);
+        // return new CreatorEventResource(true, 'List Data Posts', $data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
-use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Middleware\Authenticate;
+use App\Http\Controllers\Api\CreatorEventController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 //event-creators
-Route::apiResource('/event-creators', App\Http\Controllers\Api\CreatorEventController::class);
+Route::apiResource('/event-orgenizer', CreatorEventController::class);
